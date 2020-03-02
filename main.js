5bc8cc3a2bc1f7fa761c56d4071a8c8587af3fc3
Boris Kirov <borisasenovkirov@gmail.com>
2020-03-02 23:02:32 +0100

    testing library on

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ miro.onReady(() => {
         toolbarSvgIcon: icon,
         librarySvgIcon: icon,
 				onClick: function () {
-					miro.board.openLibrary('The Noun Project', 'library.html')
+					miro.board.openLibrary('Dribbble', 'library.html')
 				}
 			}
 		}
