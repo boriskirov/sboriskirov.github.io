25ace7c0a70c33a106cd1c760c512ebc969b9b16
Boris Kirov <borisasenovkirov@gmail.com>
2020-03-18 22:21:18 +0100

    Small fixes

--- Dribbble-test/main.js
+++ Dribbble-test/main.js
@@ -8,7 +8,7 @@ miro.onReady(() => {
         toolbarSvgIcon: icon,
         librarySvgIcon: icon,
 				onClick: function () {
-					miro.board.openLibrary('Dribbble', './library.html')
+					miro.board.openLibrary('Dribbble', '/Dribbble-test/library.html')
 				}
 			}
 		}
